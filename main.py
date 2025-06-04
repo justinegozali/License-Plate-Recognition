18aa235908d0459ef543f5ab21600aa87cc2a5f2
Vincentius Justine Gozali <145907742+justinegozali@users.noreply.github.com>
2025-06-04 12:05:38 +0700

    add send request

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import cv2
-from datetime import datetime
 import requests
+from datetime import datetime
 from detection import *
 
 GO_BACKEND_URL = 'http://localhost:3100/notification/fetch-data'  # Ganti sesuai backend Go Anda
 
-def process_frame(frame):
+def process_frame(image):
     try:
         # Deteksi kendaraan
-        detected_vehicle = detect_vehicles(frame)
+        detected_vehicle = detect_vehicles(image)
         if detected_vehicle != None:
             x1, y1, x2, y2, score, class_id = detected_vehicle
-            vehicle_crop_img = frame[int(y1):int(y2), int(x1):int(x2)]
+            vehicle_crop_img = image[int(y1):int(y2), int(x1):int(x2)]
 
             # Deteksi plat nomor
             detected_license_plate = detect_license_plates(vehicle_crop_img, class_id)
@@ -22,33 +22,34 @@ def process_frame(frame):
                 # Deteksi karakter
                 detected_character = detect_characters(license_plate_crop_img)
                 print(detected_character)
-                # if detected_character != None:                    
-                #     # Waktu pemindaian  
-                #     scanned_datetime = datetime.now().isoformat()
-
-                #     # Kirim ke backend
-                #     payload = {
-                #         'results': detected_character,
-                #         'scanned_at': scanned_datetime
-                #     }
-
-                #     response = requests.post(GO_BACKEND_URL, json=payload)
-
-                #     if response.status_code == 200:
-                #         print(f"✅ Dikirim ke backend: {detected_character} @ {scanned_datetime}")
-                #     else:
-                #         print(f"❌ Gagal kirim: Status {response.status_code}")
+                if detected_character != None:    
+                    send(vehicle_crop_img, detected_character)                
 
     except Exception as e:
-        print(f"⚠️ Error saat proses frame: {e}")
-        
+        print(f"⚠️ Error: {e}")
+
+def send(image, detected_character):
+    try:
+        # Waktu pemindaian  
+        scanned_datetime = datetime.now().isoformat()
+
+        # Kirim ke backend
+        payload = {
+            'results': detected_character,
+            'scanned_at': scanned_datetime
+        }
+
+        response = requests.post(GO_BACKEND_URL, json=payload)
+
+        if response.status_code == 200:
+            print(f"✅ Dikirim ke backend: {detected_character} @ {scanned_datetime}")
+        else:
+            print(f"❌ Gagal kirim: Status {response.status_code}")
+
+    except Exception as e:
+        print(f"⚠️ Error: {e}")
 
 def main():
-    # image = cv2.imread('./image/imagetest6.jpg')
-    # # cv2.imshow("image", image)
-
-    # process_frame(image)
-
     cap = cv2.VideoCapture(0)  # Webcam laptop atau USB
 
     # Set resolusi lebih kecil jika perlu (opsional)
@@ -77,11 +78,18 @@ def main():
         # cv2.waitKey(1000)  # tunggu 5 detik sebelum lanjut
 
         # Keluar dengan menekan 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+# for testing
+def main_test():
+    image = cv2.imread('./image/imagetest6.jpg')
+    # cv2.imshow("image", image)
+
+    process_frame(image)
 
 if __name__ == "__main__":
     main()
